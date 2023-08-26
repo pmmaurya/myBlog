1f28a12c4ed4923fc2f7a7fac8ed1512d9be76fd
Pawan <pawanmaurya22660@gmail.com>
2023-08-26 10:19:32 +0530

    Your commit message

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bv2#n5f(x5d_)gm50rab%+65*g5*vr%ty_k%j4r)fu!xjfanm4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
